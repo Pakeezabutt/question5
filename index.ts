617242928ec8511777939dcacccb4ee5e8be79e7
Pakeezabutt <139985837+Pakeezabutt@users.noreply.github.com>
2023-08-01 06:49:51 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,14 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// Heraclitus once said, “No man ever steps in the same river twice, for it's not the same river and he's not the same man.”
+
+//console.log('Heraclitus once said, “No man ever steps in the same river twice, for its not the same river and he is not the same man.”')
+
+
+let famous_person: string = "Heraclitus";
+let message: string = famous_person + ' once said, “No man ever steps in the same river twice, for its not the same river and he is not the same man.”';
+console.log(message);
